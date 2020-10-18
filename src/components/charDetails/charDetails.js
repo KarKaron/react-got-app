b71a25b384d416c1874dbd6ce844adf3e10ed465
KarKaron <karkotski@gmail.com>
2020-10-18 22:57:34 +0300

    Update styles

--- src/components/charDetails/charDetails.js
+++ src/components/charDetails/charDetails.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
-import './charDetails.css';
+import styled from 'styled-components';
+
+const CharDetail = styled.div`
+  background-color: #fff;
+  padding: 25px 25px 15px 25px;
+  margin-bottom: 40px;
+`;
+
+const CharDetailTitle = styled.h4`
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+// const selectEror = styled.span`
+//   color: #fff;
+//   text-align: center;
+//   font-size: 26px;
+// `;
 
 export default class CharDetails extends Component {
   render() {
     return (
-      <div className="char-details rounded">
-        <h4>John Snow</h4>
+      <CharDetail className="rouned">
+        <CharDetailTitle>John Snow</CharDetailTitle>
         <ul className="list-group list-group-flush">
           <li className="list-group-item d-flex justify-content-between">
             <span className="term">Gender</span>
@@ -24,7 +41,7 @@ export default class CharDetails extends Component {
             <span>First</span>
           </li>
         </ul>
-      </div>
+      </CharDetail>
     );
   }
 }
